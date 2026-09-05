2e0bd2e6b5c2d49398a87684b700373ac052add0
tejasvipundir05 <teejasvvipundiir10@gmail.com>
2026-09-05 22:18:46 +0530

    Add files via upload

---
+++ DAY27.C
@@ -0,0 +1,85 @@
+// Q53: Write a program to print the following pattern:
+// *
+// ***
+// *****
+// *******
+// *********
+// *******
+// *****
+// ***
+// *
+
+
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Increasing part
+    for (i = 1; i <= 5; i++) {
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Decreasing part
+    for (i = 4; i >= 1; i--) {
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+// Q54: Write a program to print the following pattern:
+
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 4; i++) {
+        for (j = 1; j <= 4 - i; j++)
+            printf(" ");
+        for (j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    for (i = 3; i >= 1; i--) {
+        for (j = 1; j <= 4 - i; j++)
+            printf(" ");
+        for (j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+
