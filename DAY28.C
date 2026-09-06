22f54117cd069e1d7df1d4980fb1a4a86876cdb7
tejasvipundir05 <teejasvvipundiir10@gmail.com>
2026-09-06 22:02:11 +0530

    Add files via upload

---
+++ DAY28.C
@@ -0,0 +1,61 @@
+//Q55: Write a program to print all the prime numbers from 1 to n.
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++) {
+        isPrime = 1;
+
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+//Q56: Read and print elements of a one-dimensional array.
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
